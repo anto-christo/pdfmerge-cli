d77b3266ec906912c64f42c592b85648476b29f7
anto-christo <anto.christo.20@gmail.com>
2019-04-09 14:01:20 +0530

    Add feature for suggesting pdf file names based on user input

--- scripts/prompt.js
+++ scripts/prompt.js
@@ -3,6 +3,9 @@
 const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
+const fuzzy = require('fuzzy');
+
+inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
 
 let getAllPdfFiles = () => {
   let pdfFiles = [];
@@ -12,11 +15,36 @@ let getAllPdfFiles = () => {
   return pdfFiles;
 }
 
+let getPdfSuggestions = (answers, input) => {
+  input = input || '';
+  return new Promise(function (resolve) {
+    var fuzzyResult = fuzzy.filter(input, getAllPdfFiles());
+    resolve(
+      fuzzyResult.map(function (el) {
+        return el.original;
+      })
+    );
+  });
+}
+
+let askFileName = () => {
+  return inquirer.prompt([{
+    type: 'autocomplete',
+    name: 'fileName',
+    message: 'Select a PDF file:',
+    source: getPdfSuggestions,
+    pageSize: 20
+  }]).then((choice) => {
+    return choice.fileName;
+  });
+}
+
 let askNumOfPdf = () => {
   return inquirer.prompt({
     name: 'num',
     type: 'number',
     message: 'Enter the number of PDF(s) to be merged:',
+    default: 2
   }).then((input) => {
     return input.num;
   })
@@ -24,7 +52,11 @@ let askNumOfPdf = () => {
 
 let displayPrompts = async () => {
   let num = await askNumOfPdf();
-  console.log('files', getAllPdfFiles());
+  let filesToBeMerged = [];
+  for (let i = 0; i < num; i++) {
+    filesToBeMerged.push(await askFileName());
+  }
+  console.log(filesToBeMerged);
 }
 
 module.exports = displayPrompts;
