547ba9dd11a702c60c1d884e0141cd77437edddf
anto-christo <anto.christo.20@gmail.com>
2019-04-09 16:38:52 +0530

    Add spinner and show source and destination file names

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,42 @@
 'use strict';
 
+const ora = require('ora');
 const prompt = require('./prompt')
 const mergeFiles = require('./merge');
 const ui = require('./interface');
+
+let displaySelectedFiles = filesToBeMerged => {
+  ui.showMessage('yellow', 'Source Files:');
+  filesToBeMerged.forEach(file => {
+    ui.showMessage('cyan', file);
+  });
+}
+
+let displayDestinationName = destinationFileName => {
+  ui.showMessage('yellow', '\nDestination File:');
+  ui.showMessage('cyan', destinationFileName);
+}
+
+let startMerge = async (filesToBeMerged, destinationFileName) => {
+  console.log('\n');
+  let spinner = ora('Merging your files').start();
+  try {
+    await mergeFiles(filesToBeMerged, destinationFileName);
+    spinner.succeed();
+    ui.showMessage('green', 'Successfully merged the files !!!');
+  } catch (err) {
+    spinner.fail();
+    ui.showMessage('red', `An error occured while merging\n${err}`);
+  }
+}
 
 let main = async () => {
   let filesToBeMerged = await prompt.getFilesToBeMerged();
   let destinationFileName = await prompt.askDestinationName();
   ui.showNewScreen();
-  try {
-    await mergeFiles(filesToBeMerged, destinationFileName);
-    ui.showMessage('green', 'Merging Successfull !!!');
-  } catch (err) {
-    ui.showMessage('red', `An error occured while merging\n${err}`);
-  }
+  displaySelectedFiles(filesToBeMerged);
+  displayDestinationName(destinationFileName);
+  await startMerge(filesToBeMerged, destinationFileName);
 }
 
 module.exports = main;
